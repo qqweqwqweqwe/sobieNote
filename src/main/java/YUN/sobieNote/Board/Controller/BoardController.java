a2e50c61cb2c092a3d894ae83995c701d66edd2b
k0789789 <k0789789@naver.com>
2025-01-07 23:31:05 +0900

    Feat : Add board post and patch function

--- src/main/java/YUN/sobieNote/Board/Controller/BoardController.java
+++ src/main/java/YUN/sobieNote/Board/Controller/BoardController.java
@@ -1,6 +1,7 @@
 package YUN.sobieNote.Board.Controller;
 
 
+import YUN.sobieNote.Board.DTO.BoardPostResponse;
 import YUN.sobieNote.Board.DTO.BoardRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -12,14 +13,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/board")
 public class BoardController {
 
+    /**
+     * 게시판에 피드 내용을 추가하는 함수입니다
+     * @param memberId
+     * @param request
+     * @return
+     */
     @PostMapping("/posting/{memberId}")
     @ResponseBody
-    public void createPost(
-            @PathVariable Long memberId,
+    public ResponseEntity<BoardPostResponse> createPost(
+            @PathVariable long memberId,
             @RequestBody BoardRequest request
             ){
-        
 
+        return ResponseEntity.ok()
+                .body(new BoardPostResponse("result", "msg", true));
+    }
 
+    @PatchMapping("/posting/{boardId}")
+    @ResponseBody
+    public ResponseBody<BoardPatchResponse>patchPost(
+            @PathVariable long boardId,
+            @RequestBody BoardRequest request
+            ){
+
+        return  ResponseEntity.ok()
+                .body()
     }
 }
--- src/main/java/YUN/sobieNote/Board/DTO/BoardPostResponse.java
+++ src/main/java/YUN/sobieNote/Board/DTO/BoardPostResponse.java
@@ -1,10 +1,19 @@
 package YUN.sobieNote.Board.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class BoardPostResponse {
 
     private BoardRequest boardRequest;
     private String result;
     private String msg;
-    private String data;
+    private boolean data;
 
+    public BoardPostResponse(String result, String msg, boolean data) {
+        this.result = result;
+        this.msg = msg;
+        this.data = data;
+
+    }
 }
