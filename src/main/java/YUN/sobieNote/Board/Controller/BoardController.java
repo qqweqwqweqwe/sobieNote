474bb10e76f5a556ad0e5c7e91581f1848147fa3
k0789789 <k0789789@naver.com>
2025-01-07 16:42:15 +0900

    Feat : Add create post (progress)

--- src/main/java/YUN/sobieNote/Board/Controller/BoardController.java
+++ src/main/java/YUN/sobieNote/Board/Controller/BoardController.java
@@ -1,22 +1,25 @@
 package YUN.sobieNote.Board.Controller;
 
 
+import YUN.sobieNote.Board.DTO.BoardRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("board")
+@Controller
+@RequestMapping("/board")
 public class BoardController {
 
-
-
-    @PostMapping("posting:{meberId}")
+    @PostMapping("/posting/{memberId}")
     @ResponseBody
-    public ResponseEntity<?> post(HttpServletRequest request){
-        String authorizationHeader = request.getHeader("Authorization");
+    public void createPost(
+            @PathVariable Long memberId,
+            @RequestBody BoardRequest request
+            ){
+        
 
-        if(authorizationHeader == null) || authorizationHeader.startsWith()}
 
     }
 }
