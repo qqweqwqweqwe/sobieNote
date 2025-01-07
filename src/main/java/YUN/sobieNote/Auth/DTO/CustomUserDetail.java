4742b35a6ea860acf1e7b5c581bad51aaec62d16
k0789789 <k0789789@naver.com>
2025-01-07 09:27:53 +0900

    Feat : Add loadUserByUsername

--- src/main/java/YUN/sobieNote/Auth/DTO/CustomUserDetail.java
+++ src/main/java/YUN/sobieNote/Auth/DTO/CustomUserDetail.java
@@ -1,5 +1,6 @@
 package YUN.sobieNote.Auth.DTO;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -8,14 +9,17 @@ import java.util.List;
 
 public class CustomUserDetail implements UserDetails {
 
-    private String id;	// DB에서 PK 값
-    private String loginId;		// 로그인용 ID 값
-    private String password;	// 비밀번호
+    private long id;	// DB에서 PK 값
+    private String userName;		// 로그인용 ID 값
     private String email;	//이메일
-    private boolean emailVerified;	//이메일 인증 여부
-    private boolean locked;	//계정 잠김 여부
-    private String nickname;	//닉네임
     private Collection<GrantedAuthority> authorities;	//권한 목록
+
+    public CustomUserDetail(long id, String userName, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+    }
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -29,6 +33,6 @@ public class CustomUserDetail implements UserDetails {
 
     @Override
     public String getUsername() {
-        return "";
+        return this.userName;
     }
 }
