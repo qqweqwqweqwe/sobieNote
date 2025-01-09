8b8a61cd7c35d17b8934933db8e018b4567b2f39
k0789789 <k0789789@naver.com>
2025-01-09 16:00:19 +0900

    Feat : Complete BoardGet

--- src/main/java/YUN/sobieNote/Board/Controller/BoardController.java
+++ src/main/java/YUN/sobieNote/Board/Controller/BoardController.java
@@ -1,21 +1,51 @@
 package YUN.sobieNote.Board.Controller;
 
 
-import YUN.sobieNote.Board.DTO.BoardPatchResponse;
-import YUN.sobieNote.Board.DTO.BoardPostResponse;
-import YUN.sobieNote.Board.DTO.BoardRequest;
+import YUN.sobieNote.Board.DTO.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
     /**
-     * 게시판에 피드 내용을 추가하는 함수입니다
+     * 피드 내용을 조회합니다.
+     * @return BoardGetResponse
+     */
+    @GetMapping("/posting/{boardId}")
+    @ResponseBody
+    public ResponseEntity<BoardGetResponse> getPost(
+            @PathVariable long boardId
+    ){
+        // boardId로 조회
+        //  return entity to dto
+        BoardGetResponse.Data data = new BoardGetResponse.Data(
+                new Date(),
+                "test",
+                1,
+                "test",
+                "test",
+                "test"
+        );
+        BoardGetResponse boardGetResponse = new BoardGetResponse(
+                "test",
+                "test",
+                data
+        );
+
+        return ResponseEntity.ok()
+                .body(boardGetResponse);
+    }
+
+
+    /**
+     * 게시판에 피드 내용을 추가하는 API입니다
      * @param memberId
      * @param request
      * @return
@@ -27,10 +57,17 @@ public class BoardController {
             @RequestBody BoardRequest request
             ){
 
+        BoardPostResponse boardPostResponse = new BoardPostResponse();
         return ResponseEntity.ok()
-                .body(new BoardPostResponse("result", "msg", true));
+                .body(boardPostResponse);
     }
 
+    /**
+     * 피드에 내용을 추가하는 API입니다.
+     * @param boardId
+     * @param request
+     * @return
+     */
     @PatchMapping("/posting/{boardId}")
     @ResponseBody
     public ResponseEntity<BoardPatchResponse>patchPost(
@@ -40,4 +77,7 @@ public class BoardController {
         return  ResponseEntity.ok()
                 .body(new BoardPatchResponse("result", "msg", true));
     }
+
+//    @DeleteMapping("/")
+//    public ResponseEntity
 }
--- src/main/java/YUN/sobieNote/Board/DTO/BoardGetResponse.java
+++ src/main/java/YUN/sobieNote/Board/DTO/BoardGetResponse.java
@@ -1,15 +1,24 @@
 package YUN.sobieNote.Board.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardGetResponse {
 
     private String result;
     private String msg;
     private Data data;
 
-
-    private class Data {
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Data {
         private Date createdDate;
         private String emotions;
         private long satisfactions;
