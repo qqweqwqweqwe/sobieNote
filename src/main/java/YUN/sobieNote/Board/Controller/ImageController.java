3859f23ac465e3a851b65300cb348277d8547033
k0789789 <k0789789@naver.com>
2025-01-09 19:07:57 +0900

    Feat : Add get image

--- src/main/java/YUN/sobieNote/Board/Controller/ImageController.java
+++ src/main/java/YUN/sobieNote/Board/Controller/ImageController.java
@@ -1,12 +1,47 @@
-//package YUN.sobieNote.Board.Controller;
-//
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@RequestMapping("image")
-//public class ImageController {
-//
-//    @GetMapping("")
-//
-//}
+package YUN.sobieNote.Board.Controller;
+
+
+import YUN.sobieNote.Board.DTO.ImageGetResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+
+@RequestMapping("image")
+@Controller
+@ResponseBody
+public class ImageController {
+
+    /**
+     * 이미지를 조회합니다.
+     * @param boardId 게시글 Id
+     * @return ImageGetResponse
+     */
+    @GetMapping("/{boardId}")
+    public ResponseEntity<ImageGetResponse> getImage(
+            @PathVariable long boardId
+    ){
+        ImageGetResponse.Data data= new ImageGetResponse.Data(
+                "test",
+                new Date(),
+                "test",
+                "test",
+                1,
+                "test"
+        );
+        ImageGetResponse imageGetResponse = new ImageGetResponse(
+                "test",
+                "test",
+                data
+        );
+        return ResponseEntity.ok()
+                .body(imageGetResponse);
+
+    }
+
+}
