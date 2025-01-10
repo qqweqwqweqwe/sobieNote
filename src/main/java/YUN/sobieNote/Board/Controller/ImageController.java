7662dda7fce10262676887e66bc5da786a05a634
k0789789 <k0789789@naver.com>
2025-01-10 17:53:14 +0900

    Feat : Add get image by member

--- src/main/java/YUN/sobieNote/Board/Controller/ImageController.java
+++ src/main/java/YUN/sobieNote/Board/Controller/ImageController.java
@@ -2,6 +2,7 @@ package YUN.sobieNote.Board.Controller;
 
 
 import YUN.sobieNote.Board.DTO.ImageGetResponse;
+import YUN.sobieNote.Board.DTO.ImageGetResponseByMember;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 @RequestMapping("image")
@@ -45,6 +48,18 @@ public class ImageController {
     }
 
     @GetMapping("/{year}/{month}/{memberId}")
-    public ResponseEntity<>
+    public ResponseEntity<ImageGetResponseByMember> getImageByMember(
+            @PathVariable long year,
+            @PathVariable long month,
+            @PathVariable long memberId
+    ){
+        ImageGetResponseByMember.Data data= new ImageGetResponseByMember.Data(new ArrayList<String>(List.of(new String[]{"test"})));
+        return ResponseEntity.ok()
+                .body(new ImageGetResponseByMember(
+                        "test",
+                        "test",
+                        data
+                        ));
+    }
 
 }
