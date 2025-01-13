1da9eac7d5cd541560ec501ed6221ad24dfef069
k0789789 <k0789789@naver.com>
2025-01-13 23:12:37 +0900

    Fix : Rename CategoryReport to CategoryMonthReport

--- src/main/java/YUN/sobieNote/Report/DTO/CategoryMonthReportGetResponse.java
+++ src/main/java/YUN/sobieNote/Report/DTO/CategoryMonthReportGetResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CategoryReportGetResponse {
+public class CategoryMonthReportGetResponse {
     private String result;
     private String msg;
     private Data data;
