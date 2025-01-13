a590d1225264b9e80c118cfe6b776219751d8521
k0789789 <k0789789@naver.com>
2025-01-13 23:11:15 +0900

    Fix : Rename EmotionReport to EmotionMonthReport

--- src/main/java/YUN/sobieNote/Report/DTO/EmotionsMonthReportGetResponse.java
+++ src/main/java/YUN/sobieNote/Report/DTO/EmotionsMonthReportGetResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class EmotionsReportGetResponse {
+public class EmotionsMonthReportGetResponse {
 
     private String result;
     private String msg;
