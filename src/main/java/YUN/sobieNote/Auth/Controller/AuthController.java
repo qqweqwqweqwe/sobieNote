f023998a5e8f8d22b8ee234ec8a49aeabbcd2118
k0789789 <k0789789@naver.com>
2025-01-15 04:09:35 +0900

    Feat : Add auth controller

---
+++ src/main/java/YUN/sobieNote/Auth/Controller/AuthController.java
@@ -0,0 +1,11 @@
+package YUN.sobieNote.Auth.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+}
