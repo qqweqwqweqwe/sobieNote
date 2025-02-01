3bde3142eeaec1c0b62310cc4565da9016170af5
k0789789 <k0789789@naver.com>
2025-02-01 20:57:06 +0900

    Feat : Create login error response dto

--- src/main/java/YUN/sobieNote/Member/Exception/MemberLoginErrorResponse.java
+++ src/main/java/YUN/sobieNote/Member/Exception/MemberLoginErrorResponse.java
@@ -2,5 +2,13 @@ package YUN.sobieNote.Member.Exception;
 
 public class MemberLoginErrorResponse {
 
+    private String code;
+    private String message;
+
+
+    public MemberLoginErrorResponse(String code, String message ){
+        this.code  = code;
+        this.message = message;
+    }
 
 }
