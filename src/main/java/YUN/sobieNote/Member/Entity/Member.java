af451b23d084524ca986f8e6d3b5d68769f1c712
k0789789 <k0789789@naver.com>
2025-02-01 20:56:27 +0900

    Feat : Create member entity

--- src/main/java/YUN/sobieNote/Member/Entity/Member.java
+++ src/main/java/YUN/sobieNote/Member/Entity/Member.java
@@ -1,11 +1,15 @@
 package YUN.sobieNote.Member.Entity;
 
+import YUN.sobieNote.Board.Entity.Board;
+import YUN.sobieNote.Board.Entity.Meaning;
+import YUN.sobieNote.Member.DTO.MemberLoginRequest;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,18 +17,26 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "members")
+@Table(name = "member")
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment 데이터베이스에서 자동으로 넣어줌
-    private long id;
+    private int id;
 
     @Column(nullable = true)
     private String name;
 
     @Column(nullable = false) // 고유(unique) 및 Not Null 설정
     private String email;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn(name = "role_id")
+    private MemberRole memberRole;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn(name = "auth_provider_id")
+    private AuthProvider authProvider;
 
     @Column(name = "created_at", updatable = false) // 열 이름을 지정하고 수정 불가능
     @CreationTimestamp
@@ -34,5 +46,15 @@ public class Member {
     @UpdateTimestamp
     private LocalDateTime updatedAt = LocalDateTime.now();
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Board> boards;
 
+
+    public Member(MemberLoginRequest memberLoginRequest, AuthProvider authProvider,MemberRole memberRole) {
+        this.name = memberLoginRequest.getName();
+        this.email = memberLoginRequest.getEmail();
+        this.authProvider = authProvider;
+        this.memberRole = memberRole;
+
+    }
 }
