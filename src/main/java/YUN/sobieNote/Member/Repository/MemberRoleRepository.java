f4a1ebfdb79e89c0954c1c1fcc34ad75c4d3b603
k0789789 <k0789789@naver.com>
2025-02-01 20:58:16 +0900

    Feat : Create member role repository

---
+++ src/main/java/YUN/sobieNote/Member/Repository/MemberRoleRepository.java
@@ -0,0 +1,7 @@
+package YUN.sobieNote.Member.Repository;
+
+import YUN.sobieNote.Member.Entity.MemberRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRoleRepository extends JpaRepository<MemberRole,Integer> {
+}
