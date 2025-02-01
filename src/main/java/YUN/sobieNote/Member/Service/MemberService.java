3d389938b23826964cdf2d3d318dfbe9cbe74f83
k0789789 <k0789789@naver.com>
2025-02-01 20:59:22 +0900

    Feat : Create auth member service

---
+++ src/main/java/YUN/sobieNote/Member/Service/MemberService.java
@@ -0,0 +1,46 @@
+package YUN.sobieNote.Member.Service;
+
+import YUN.sobieNote.Member.DTO.MemberLoginRequest;
+import YUN.sobieNote.Member.DTO.MemberLoginResponse;
+import YUN.sobieNote.Member.Entity.AuthProvider;
+import YUN.sobieNote.Member.Entity.Member;
+import YUN.sobieNote.Member.Entity.MemberRole;
+import YUN.sobieNote.Member.Repository.AuthProviderRepository;
+import YUN.sobieNote.Member.Repository.MemberRepository;
+import YUN.sobieNote.Member.Repository.MemberRoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final AuthProviderRepository authProviderRepository;
+    private final MemberRoleRepository memberRoleRepository;
+
+
+    public Member findMemberByEmail(String email ){
+        return memberRepository.findByEmail(email)
+                .orElseThrow(()-> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. Email: " + email));
+    }
+
+
+    public Member findOrSaveMember(MemberLoginRequest memberLoginRequest, int authProviderId, int memberRoleId) {
+
+        AuthProvider authProvider = authProviderRepository.findById(authProviderId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 소셜 로그인 방식이 없습니다. auth_provider_id: " + authProviderId));
+
+        MemberRole memberRole = memberRoleRepository.findById(memberRoleId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저 권한을 찾을 수 없습니다. member_role_id: " + memberRoleId));
+
+
+        return memberRepository.findByEmail(memberLoginRequest.getEmail())
+                .orElseGet(() -> {
+                    Member newMember = new Member(memberLoginRequest,authProvider,memberRole);
+                    return memberRepository.save(newMember);
+                });
+    }
+
+
+}
