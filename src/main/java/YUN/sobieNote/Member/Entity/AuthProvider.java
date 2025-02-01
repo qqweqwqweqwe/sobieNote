912c08bc67320d438388f5c368f916b634a1547e
k0789789 <k0789789@naver.com>
2025-02-01 21:01:08 +0900

    Feat : Add member extra entity

---
+++ src/main/java/YUN/sobieNote/Member/Entity/AuthProvider.java
@@ -0,0 +1,23 @@
+package YUN.sobieNote.Member.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "auth_provider")
+public class AuthProvider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment 데이터베이스에서 자동으로 넣어줌
+    private int id;
+
+    @Column(name = "auth_provider")
+    private String authProvider;
+
+
+}
---
+++ src/main/java/YUN/sobieNote/Member/Entity/MemberRole.java
@@ -0,0 +1,23 @@
+package YUN.sobieNote.Member.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "member_role")
+public class MemberRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment 데이터베이스에서 자동으로 넣어줌
+    private int id;
+
+    @Column(nullable = true)
+    private String role;
+
+
+}
