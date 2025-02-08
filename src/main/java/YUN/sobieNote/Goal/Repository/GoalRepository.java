6f2706b9ab672108add0d74d4f16e124051eca50
k0789789 <k0789789@naver.com>
2025-02-08 16:06:39 +0900

    Feat : Add goal repository

--- src/main/java/YUN/sobieNote/Goal/Repository/GoalRepository.java
+++ src/main/java/YUN/sobieNote/Goal/Repository/GoalRepository.java
@@ -3,7 +3,9 @@ package YUN.sobieNote.Goal.Repository;
 import YUN.sobieNote.Goal.Entity.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GoalRepository extends JpaRepository<Goal,Integer> {
 
-
+    Optional<Goal> findByMemberId(int memberId);
 }
