79d1384cfd04c93b9f6be31eab619566ab6ae0d5
k0789789 <k0789789@naver.com>
2025-02-08 15:48:52 +0900

    Feat : Add update contents

---
+++ src/main/java/YUN/sobieNote/Goal/Entity/Goal.java
@@ -0,0 +1,48 @@
+package YUN.sobieNote.Goal.Entity;
+
+import YUN.sobieNote.Member.Entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "goals")
+public class Goal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment 데이터베이스에서 자동으로 넣어줌
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "contents", nullable = false)
+    private String contents;
+
+    @Column(name = "year", nullable = false)
+    private int year;
+
+    @Column(name = "month", nullable = false)
+    private int month;
+
+    @Column(name = "created_at", updatable = false) // 열 이름을 지정하고 수정 불가능
+    @CreationTimestamp
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+
+    public void updateContents(String mission){
+        this.contents = mission;
+    }
+}
