66bfe942d1318ba0521bbb87d7bb6f6df86683ac
k0789789 <k0789789@naver.com>
2025-02-08 16:04:42 +0900

    Feat : Add constructor

--- src/main/java/YUN/sobieNote/Goal/DTO/GoalPostResponse.java
+++ src/main/java/YUN/sobieNote/Goal/DTO/GoalPostResponse.java
@@ -1,13 +1,31 @@
 package YUN.sobieNote.Goal.DTO;
 
+import YUN.sobieNote.Goal.Entity.Goal;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 public class GoalPostResponse {
     private String msg;
-    private String goalId;
+    private int goalId;
     private int year;
     private int month;
-    private long memberId;
+    private int memberId;
 
+    public GoalPostResponse(Goal goal) {
+        this.msg = "Success";
+        this.goalId = goal.getId();
+        this.year = goal.getYear();
+        this.month = goal.getMonth();
+        this.memberId = goal.getMember().getId();
+    }
+
+    public GoalPostResponse(Goal goal, String msg, int memberId) {
+        this.msg = msg;
+        this.goalId = -1;
+        this.year = -1;
+        this.month = -1;
+        this.memberId = memberId;
+    }
 }
