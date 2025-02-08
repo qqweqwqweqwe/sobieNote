1133dc2ca8b089d2fe791f3b22b359bea352ec4f
k0789789 <k0789789@naver.com>
2025-02-08 16:05:49 +0900

    Feat : Add update

--- src/main/java/YUN/sobieNote/Goal/Service/GoalService.java
+++ src/main/java/YUN/sobieNote/Goal/Service/GoalService.java
@@ -1,6 +1,7 @@
 package YUN.sobieNote.Goal.Service;
 
 import YUN.sobieNote.Goal.DTO.GoalGetResponse;
+import YUN.sobieNote.Goal.DTO.GoalPostResponse;
 import YUN.sobieNote.Goal.Entity.Goal;
 import YUN.sobieNote.Goal.Repository.GoalRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +13,20 @@ public class GoalService {
     private final GoalRepository goalRepository;
 
     public GoalGetResponse getGoalByMemberId(int memberId){
-        Goal goal = goalRepository.findById(memberId)
-                .orElseThrow(()-> new IllegalArgumentException("해당 목표를 찾을 수 없습니다 memberId: " +memberId ));
+        Goal goal = goalRepository.findByMemberId(memberId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 유저의 목표를 찾을 수 없습니다 memberId: " +memberId ));
 
         return new GoalGetResponse(goal);
 
+    }
 
+    public GoalPostResponse updateGoalByMemberId(int memberId, String mission){
+        Goal goal = goalRepository.findByMemberId(memberId)
+                .orElseThrow(()-> new IllegalArgumentException("해당 유저의 목표를 찾을 수 없습니다. memberId: " +memberId ));
 
+        goal.updateContents(mission);
+
+        return new GoalPostResponse(goal);
     }
 
 }
