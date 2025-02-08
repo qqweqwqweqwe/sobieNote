4fafb813f95d8888486f5dcd2703259e15608c27
k0789789 <k0789789@naver.com>
2025-02-08 16:04:20 +0900

    Feat : Add goal api

--- src/main/java/YUN/sobieNote/Goal/Controller/GoalController.java
+++ src/main/java/YUN/sobieNote/Goal/Controller/GoalController.java
@@ -2,32 +2,49 @@ package YUN.sobieNote.Goal.Controller;
 
 
 import YUN.sobieNote.Goal.DTO.GoalGetResponse;
+import YUN.sobieNote.Goal.DTO.GoalPostRequest;
 import YUN.sobieNote.Goal.DTO.GoalPostResponse;
+import YUN.sobieNote.Goal.Service.GoalService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/goal")
+@RequiredArgsConstructor
 public class GoalController {
 
+    private final GoalService goalService;
     @GetMapping("/{memberId}")
     public ResponseEntity<GoalGetResponse> getGoal(
-            @PathVariable long memberId
+            @PathVariable int memberId
     ){
-        return ResponseEntity.ok()
-                .body(new GoalGetResponse());
+        try {
+            GoalGetResponse goalGetResponse = goalService.getGoalByMemberId(memberId);
+            return ResponseEntity.ok()
+                    .body(goalGetResponse);
+        } catch (Exception e) {
+            return  ResponseEntity.badRequest()
+                    .body(new GoalGetResponse(null,"FAIL", e.getMessage()));
+        }
     }
 
     @PostMapping("/{memberId}")
     public ResponseEntity<GoalPostResponse> modifyGoal(
-            @PathVariable long memberId
-    ){
-        return ResponseEntity.ok()
-                .body(new GoalPostResponse());
+            @PathVariable int memberId,
+            @RequestBody GoalPostRequest goalPostRequest
+            ){
+
+        try {
+            GoalPostResponse goalPostResponse = goalService.updateGoalByMemberId(memberId, goalPostRequest.getMission());
+            return ResponseEntity.ok()
+                    .body(goalPostResponse);
+        }catch (Exception e){
+            return ResponseEntity.badRequest()
+                    .body(new GoalPostResponse(null,"FAIL",memberId));
+        }
+
     }
 
 }
