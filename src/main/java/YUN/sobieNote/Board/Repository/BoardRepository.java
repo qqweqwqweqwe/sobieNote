43a2b86036ce88ca66efba0ffaaf78a1f40238c6
k0789789 <k0789789@naver.com>
2025-02-09 21:51:51 +0900

    Feat : Add findBoard

--- src/main/java/YUN/sobieNote/Board/Repository/BoardRepository.java
+++ src/main/java/YUN/sobieNote/Board/Repository/BoardRepository.java
@@ -2,6 +2,8 @@ package YUN.sobieNote.Board.Repository;
 
 import YUN.sobieNote.Board.Entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,17 @@ import java.util.Optional;
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Integer> {
     List<Board> findByMemberIdAndCreatedAtYearAndCreatedAtMonth(int year, int month,int memberId);
+
+    @Query("SELECT b FROM Board b WHERE " +
+            "(:memberId IS NULL OR b.memberId = :memberId) " +
+            "AND (:year IS NULL OR FUNCTION('YEAR', b.createdAtYear) = :year) " +
+            "AND (:month IS NULL OR FUNCTION('MONTH', b.createdAtMonth) = :month)")
+    Optional<List<Board>> findBoards(
+            @Param("memberId") Integer memberId,
+            @Param("year") Integer year,
+            @Param("month") Integer month
+    );
+
 
 
 }
