f091ce8f204a3f074f16bf647a0c400821a02b67
k0789789 <k0789789@naver.com>
2025-02-10 21:59:50 +0900

    Feat : Add annotation and constructor

--- src/main/java/YUN/sobieNote/Report/DTO/ReportCategoryGetResponse.java
+++ src/main/java/YUN/sobieNote/Report/DTO/ReportCategoryGetResponse.java
@@ -4,8 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReportCategoryGetResponse extends ReportGetResponse{
+    public ReportCategoryGetResponse(String result, String msg, List<Data> data) {
+        super(result,msg,data);
+    }
 }
--- src/main/java/YUN/sobieNote/Report/DTO/ReportGetResponse.java
+++ src/main/java/YUN/sobieNote/Report/DTO/ReportGetResponse.java
@@ -4,9 +4,14 @@ import lombok.AllArgsConstructor;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReportGetResponse {
 
     private String result;
