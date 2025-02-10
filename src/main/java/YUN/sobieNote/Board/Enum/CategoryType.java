7f66841a984165aa529a6c3681ad63e89bf61df2
k0789789 <k0789789@naver.com>
2025-02-10 21:58:28 +0900

    Feat : Add get category report

--- src/main/java/YUN/sobieNote/Board/Enum/CategoryType.java
+++ src/main/java/YUN/sobieNote/Board/Enum/CategoryType.java
@@ -1,5 +1,5 @@
 package YUN.sobieNote.Board.Enum;
 
 public enum CategoryType {
-    FOOD
+    FOOD // 이런식으로 하면 enum.food 로 참조하면 db에는 0이 들어간다고 하네용
 }
--- src/main/java/YUN/sobieNote/Report/Service/ReportService.java
+++ src/main/java/YUN/sobieNote/Report/Service/ReportService.java
@@ -1,14 +1,43 @@
 package YUN.sobieNote.Report.Service;
 
 
+import YUN.sobieNote.Board.Entity.Board;
+import YUN.sobieNote.Board.Service.BoardService;
 import YUN.sobieNote.Report.DTO.ReportCategoryGetResponse;
+import YUN.sobieNote.Report.DTO.ReportGetResponse;
+import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
 public class ReportService {
 
-
+    private final BoardService boardService;
 
 
     public ReportCategoryGetResponse getReportCategory(int memberId, int year, Integer month){
+        try {
+            List<Board> boards = boardService.getPosts(memberId, year, month);
+            ReportCategoryGetResponse reportCategoryGetResponse = new ReportCategoryGetResponse();
 
+            Map<String, Integer> categoryCount = new HashMap<>();
+
+            for (Board bo : boards) {
+                String category = bo.getCategory().getType().name();
+                categoryCount.put(category, categoryCount.getOrDefault(category, 0) + 1);
+            }
+
+            for (String key : categoryCount.keySet()) {
+                reportCategoryGetResponse.getData().add(new ReportGetResponse.Data(key, categoryCount.get(key)));
+            }
+
+            reportCategoryGetResponse.setMsg("Success");
+            reportCategoryGetResponse.setResult("OK");
+            return reportCategoryGetResponse;
+        }catch (Exception e){
+            return new ReportCategoryGetResponse("FAIL", "조회에 실패하였습니다 member Id : " + memberId,null);
+        }
     }
 }
