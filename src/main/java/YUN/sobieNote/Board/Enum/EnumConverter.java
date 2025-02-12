a8440df1e74082db405041fa68b405b8d4c3fdb0
k0789789 <k0789789@naver.com>
2025-02-12 14:41:06 +0900

    Feat : Add annotaion to EnumConverter

--- src/main/java/YUN/sobieNote/Board/Enum/EnumConverter.java
+++ src/main/java/YUN/sobieNote/Board/Enum/EnumConverter.java
@@ -1,5 +1,8 @@
 package YUN.sobieNote.Board.Enum;
 
+
+// 문자열을 enum의 정수형으로 바꿔주는 converter
+// ex) input(스포츠) -> output(1)
 public class EnumConverter {
 
     public static CategoryType convertStringToCategory(String str){
@@ -10,4 +13,7 @@ public class EnumConverter {
         return EmotionType.valueOf(str);
     }
 
+    public static FactorType convertStringToFactor(String str){
+        return FactorType.valueOf(str);
+    }
 }
