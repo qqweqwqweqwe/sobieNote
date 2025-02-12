31304141e1ea838aa183cc16b7cc656cabce9364
k0789789 <k0789789@naver.com>
2025-02-12 14:44:08 +0900

    Feat : Add factor repository

---
+++ src/main/java/YUN/sobieNote/Board/Repository/FactorRepository.java
@@ -0,0 +1,7 @@
+package YUN.sobieNote.Board.Repository;
+
+import YUN.sobieNote.Board.Entity.Factor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FactorRepository extends JpaRepository<Factor,Integer> {
+}
