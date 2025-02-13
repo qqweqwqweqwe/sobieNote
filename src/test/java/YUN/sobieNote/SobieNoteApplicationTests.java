2f55471118477be04759060afaa3b235ccb0aa8d
k0789789 <k0789789@naver.com>
2025-02-13 19:21:14 +0900

    Feat : Set up for test code (progress)

--- src/test/java/YUN/sobieNote/SobieNoteApplicationTests.java
+++ src/test/java/YUN/sobieNote/SobieNoteApplicationTests.java
@@ -1,13 +1,22 @@
 package YUN.sobieNote;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest
 class SobieNoteApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("dsadasd");
+	}
+
+	@Test
+	public void test(){
+
 	}
 
 }
