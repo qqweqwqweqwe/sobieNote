a22de1c8f49d07c46fc0d0bfde47734c5aa30e7c
k0789789 <k0789789@naver.com>
2025-02-13 19:22:04 +0900

    Feat : Add seed

---
+++ src/main/java/YUN/sobieNote/Member/Seeder/MemberSeeder.java
@@ -0,0 +1,52 @@
+package YUN.sobieNote.Member.Seeder;
+
+
+import YUN.sobieNote.Auth.Controller.AuthController;
+import YUN.sobieNote.Board.Entity.Category;
+import YUN.sobieNote.Member.Entity.AuthProvider;
+import YUN.sobieNote.Member.Entity.Member;
+import YUN.sobieNote.Member.Entity.MemberRole;
+import YUN.sobieNote.Member.Repository.AuthProviderRepository;
+import YUN.sobieNote.Member.Repository.MemberRepository;
+import YUN.sobieNote.Member.Repository.MemberRoleRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class MemberSeeder {
+    private final MemberRepository memberRepository;
+    private final MemberRoleRepository memberRoleRepository;
+    private final AuthProviderRepository authProviderRepository;
+
+
+    @PostConstruct
+    @Transactional
+    public void initMembers() {
+
+        try {
+            MemberRole memberRole = memberRoleRepository.save(new MemberRole(0, "USER"));
+
+            AuthProvider authProvider = authProviderRepository.save(new AuthProvider(0, "KAKAO"));
+
+            // 빌더 패턴 쓰는 이유
+            Member member = Member.builder()
+                    .id(0)
+                    .name("테스트")
+                    .email("test@test.com")
+                    .memberRole(memberRole)
+                    .authProvider(authProvider)
+                    .build();
+
+            memberRepository.save(member);
+        }
+        catch (Exception e){
+
+        }
+    }
+
+}
