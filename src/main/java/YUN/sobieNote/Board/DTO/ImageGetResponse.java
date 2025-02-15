414a0e8fde762499bbc498610738527613b60e22
k0789789 <k0789789@naver.com>
2025-02-15 09:21:09 +0900

    Refactor : Change attribute

--- src/main/java/YUN/sobieNote/Board/DTO/ImageGetResponse.java
+++ src/main/java/YUN/sobieNote/Board/DTO/ImageGetResponse.java
@@ -13,36 +13,5 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImageGetResponse {
-    private String result;
-    private String msg;
-    private Data data;
-
-    public ImageGetResponse(Board board, String result, String msg) {
-        this.data = new Data(board);
-        this.result = result;
-        this.msg = msg;
-    }
-
-
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Data {
-        private String contents;
-        private LocalDateTime createdDate;
-        private String categories;
-        private String emotions;
-        private long satisfactions;
-        private String factors;
-
-        public Data(Board board) {
-            this.emotions = board.getEmotion().getName();
-            this.categories = board.getCategory().getName();
-            this.contents = board.getContents();
-            this.satisfactions = board.getSatisfaction();
-            this.createdDate = board.getCreatedAt();
-
-        }
-    }
-
+    private String imagePath;
 }
