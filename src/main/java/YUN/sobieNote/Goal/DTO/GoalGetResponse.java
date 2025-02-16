176a21011d5e883c4c15878b3d65e64f44de9d5a
k0789789 <k0789789@naver.com>
2025-02-16 16:54:22 +0900

    Feat : Add getter

--- src/main/java/YUN/sobieNote/Goal/DTO/GoalGetResponse.java
+++ src/main/java/YUN/sobieNote/Goal/DTO/GoalGetResponse.java
@@ -2,10 +2,12 @@ package YUN.sobieNote.Goal.DTO;
 
 import YUN.sobieNote.Goal.Entity.Goal;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class GoalGetResponse {
     private String data;
 
