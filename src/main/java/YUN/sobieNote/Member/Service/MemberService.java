db2bca3970b8d47c042fea28e88ed0a0943055ac
k0789789 <k0789789@naver.com>
2025-02-17 18:54:16 +0900

    Feat : Add login exception

--- src/main/java/YUN/sobieNote/Member/Service/MemberService.java
+++ src/main/java/YUN/sobieNote/Member/Service/MemberService.java
@@ -8,7 +8,9 @@ import YUN.sobieNote.Member.Entity.MemberRole;
 import YUN.sobieNote.Member.Repository.AuthProviderRepository;
 import YUN.sobieNote.Member.Repository.MemberRepository;
 import YUN.sobieNote.Member.Repository.MemberRoleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.InternalException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,20 +28,24 @@ public class MemberService {
     }
 
 
-    public Member findOrSaveMember(MemberLoginRequest memberLoginRequest, int authProviderId, int memberRoleId) {
+    public MemberLoginResponse findOrSaveMember(MemberLoginRequest memberLoginRequest, int authProviderId, int memberRoleId) {
 
-        AuthProvider authProvider = authProviderRepository.findById(authProviderId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 소셜 로그인 방식이 없습니다. auth_provider_id: " + authProviderId));
+        try {
 
-        MemberRole memberRole = memberRoleRepository.findById(memberRoleId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 유저 권한을 찾을 수 없습니다. member_role_id: " + memberRoleId));
+            AuthProvider authProvider = authProviderRepository.findById(authProviderId)
+                    .orElseThrow(() -> new EntityNotFoundException("해당 소셜 로그인 방식이 없습니다. auth_provider_id: " + authProviderId));
 
+            MemberRole memberRole = memberRoleRepository.findById(memberRoleId)
+                    .orElseThrow(() -> new EntityNotFoundException("해당 유저 권한을 찾을 수 없습니다. member_role_id: " + memberRoleId));
 
-        return memberRepository.findByEmail(memberLoginRequest.getEmail())
-                .orElseGet(() -> {
-                    Member newMember = new Member(memberLoginRequest,authProvider,memberRole);
-                    return memberRepository.save(newMember);
-                });
+            Member member = memberRepository.findByEmail(memberLoginRequest.getEmail())
+                    .orElseGet(() -> memberRepository.save(new Member(memberLoginRequest, authProvider, memberRole)));
+
+            return new MemberLoginResponse(member.getId());
+        }
+        catch (Exception e){
+            throw new InternalException("서버 내부 에러가 발생하였습니다 e : " + e.getMessage());
+        }
     }
 
 
